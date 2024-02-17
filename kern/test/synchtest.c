408b5b997c37828190a41a835bb41ef35f95f8bf
Rohit Gupta <gupta3@gmail.com>
2024-02-17 14:01:57 +0000

    just put old semtest in place of rwtest to make sure it runs in menu

--- kern/test/synchtest.c
+++ kern/test/synchtest.c
@@ -889,3 +889,128 @@ cvtest5(int nargs, char **args) {
 
 	return 0;
 }
+
+// static
+// void
+// rwtestthread(void *junk, unsigned long num)
+// {
+// 	// copy of semtestthread
+// 	(void)junk;
+// 
+// 	int i;
+// 
+// 	random_yielder(4);
+// 
+// 	/*
+// 	 * Only one of these should print at a time.
+// 	 */
+// 	P(testsem);
+// 	semtest_current = num;
+// 
+// 	kprintf_n("Thread %2lu: ", num);
+// 	for (i=0; i<NSEMLOOPS; i++) {
+// 		kprintf_t(".");
+// 		kprintf_n("%2lu", num);
+// 		random_yielder(4);
+// 		failif((semtest_current != num));
+// 	}
+// 	kprintf_n("\n");
+// 
+// 	V(donesem);
+// }
+
+
+// int
+// rwtest(int nargs, char **args)
+// {
+// 
+// 	// copy of semtest
+// 	(void)nargs;
+// 	(void)args;
+// 
+// 	int i, result;
+// 
+// 	kprintf_n("Starting sem1...\n");
+// 	for (i=0; i<CREATELOOPS; i++) {
+// 		kprintf_t(".");
+// 		testsem = sem_create("testsem", 2);
+// 		if (testsem == NULL) {
+// 			panic("sem1: sem_create failed\n");
+// 		}
+// 		donesem = sem_create("donesem", 0);
+// 		if (donesem == NULL) {
+// 			panic("sem1: sem_create failed\n");
+// 		}
+// 		if (i != CREATELOOPS - 1) {
+// 			sem_destroy(testsem);
+// 			sem_destroy(donesem);
+// 		}
+// 	}
+// 	spinlock_init(&status_lock);
+// 	test_status = TEST161_SUCCESS;
+// 
+// 	kprintf_n("If this hangs, it's broken: ");
+// 	P(testsem);
+// 	P(testsem);
+// 	kprintf_n("OK\n");
+// 	kprintf_t(".");
+// 
+// 	for (i=0; i<NTHREADS; i++) {
+// 		kprintf_t(".");
+// 		result = thread_fork("semtest", NULL, semtestthread, NULL, i);
+// 		if (result) {
+// 			panic("sem1: thread_fork failed: %s\n",
+// 			      strerror(result));
+// 		}
+// 	}
+// 	for (i=0; i<NTHREADS; i++) {
+// 		kprintf_t(".");
+// 		V(testsem);
+// 		P(donesem);
+// 	}
+// 
+// 	sem_destroy(testsem);
+// 	sem_destroy(donesem);
+// 	testsem = donesem = NULL;
+// 
+// 	kprintf_t("\n");
+// 	success(test_status, SECRET, "sem1");
+// 
+// 	return 0;
+// 
+// 	// copy of locktest
+// 	// int i, result;
+// 
+// 	// kprintf_n("Starting lt1...\n");
+// 	// for (i=0; i<CREATELOOPS; i++) {
+// 	// 	kprintf_t(".");
+// 	// 	testlock = lock_create("testlock");
+// 	// 	if (testlock == NULL) {
+// 	// 		panic("lt1: lock_create failed\n");
+// 	// 	}
+// 	// 	donesem = sem_create("donesem", 0);
+// 	// 	if (donesem == NULL) {
+// 	// 		panic("lt1: sem_create failed\n");
+// 	// 	}
+// 	// 	if (i != CREATELOOPS - 1) {
+// 	// 		lock_destroy(testlock);
+// 	// 		sem_destroy(donesem);
+// 	// 	}
+// 	// }
+// 	// spinlock_init(&status_lock);
+// 	// test_status = TEST161_SUCCESS;
+// 
+// 	// for (i=0; i<NTHREADS; i++) {
+// 	// 	kprintf_t(".");
+// 	// 	result = thread_fork("synchtest", NULL, locktestthread, NULL, i);
+// 	// 	if (result) {
+// 	// 		panic("lt1: thread_fork failed: %s\n", strerror(result));
+// 	// 	}
+// 	// }
+// 	// for (i=0; i<NTHREADS; i++) {
+// 	// 	kprintf_t(".");
+// 	// 	P(donesem);
+// 	// }
+// 
+// }
+
